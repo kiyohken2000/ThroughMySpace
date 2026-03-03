beb890a173acf97132dbfb202d4c2d35abdd8e70
kiyohken2000 <www.broadwell.com@gmail.com>
2026-03-03 19:12:40 +0900

    Initial Commit

---
+++ ThroughMySpace/ThroughMySpaceApp.swift
@@ -0,0 +1,33 @@
+//
+//  ThroughMySpaceApp.swift
+//  ThroughMySpace
+//
+//  Created by admin on 2026/03/03.
+//
+
+import SwiftUI
+
+@main
+struct ThroughMySpaceApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
